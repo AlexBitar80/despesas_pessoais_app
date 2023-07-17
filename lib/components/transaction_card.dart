e917adabd490cc2875a2575fd6692aec43c55539
joão Alexandre Bitar <joaoalexbitar@brq.com>
2023-07-17 17:02:07 -0300

    feat: creating card to list title, transaction value and adjusting textfield to return typed value

---
+++ lib/components/transaction_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionCard extends StatelessWidget {
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  const TransactionCard({
+    super.key,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              child: Text(
+                'R\$ ${amount.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14.0,
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  DateFormat('d MMM y').format(date),
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,45 @@
+import 'package:despesas_pessoais/components/transaction_card.dart';
+import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({
+    super.key,
+  });
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final _transactions = <Transaction>[
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      amount: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      amount: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de Água',
+      amount: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Conta de Telefone',
+      amount: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   // This widget is the root of your application.
   @override
@@ -16,18 +50,65 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Despesas Pessoais'),
         ),
-        body: const Center(
+        body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Card(
+              const Card(
                 color: Colors.blue,
                 child: Text('Gráfico'),
               ),
-              Card(
-                child: Text('Lista de Transações'),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: _transactions
+                    .map(
+                      (transaction) => TransactionCard(
+                        title: transaction.title,
+                        amount: transaction.amount,
+                        date: transaction.date,
+                      ),
+                    )
+                    .toList(),
               ),
+              Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: titleController,
+                        decoration: const InputDecoration(
+                          labelText: 'Título',
+                        ),
+                      ),
+                      TextField(
+                        controller: valueController,
+                        decoration: const InputDecoration(
+                          labelText: 'Valor (R\$)',
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            child: const Text(
+                              'Nova Transação',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onPressed: () => {
+                              print(titleController.text),
+                              print(valueController.text),
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
