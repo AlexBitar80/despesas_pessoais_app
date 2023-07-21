33323280035bfcca57192cfa3cc16cbdc4ae6315
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-07-21 16:50:09 -0300

    feat: image added when transactions do not exist

--- lib/main.dart
+++ lib/main.dart
@@ -59,19 +59,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final valueController = TextEditingController();
 
-  final _transactions = <Transaction>[
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      amount: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      amount: 211.30,
-      date: DateTime.now(),
-    ),
+  final List<Transaction> _transactions = <Transaction>[
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo Tênis de Corrida',
+    //   amount: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Conta de Luz',
+    //   amount: 211.30,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   _addTransaction(String title, double amount) {
